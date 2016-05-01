70ccbab96078a2f7a16f8d24397a5e16bb799784
Daniel Mai <daniel@danielmai.net>
2016-05-01 16:02:10 -0700

    Add OPTIONS preflight route.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,5 +9,10 @@ public class Application extends Controller {
 
     public Result index() {
         return ok(index.render("Your new application is ready."));
-    }   
+    }
+
+    public Result preflight() {
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        return ok();
+    }
 }
