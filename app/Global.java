f94b83ee8796b34860a97ba28540722c7c4979d3
Daniel Mai <daniel@danielmai.net>
2016-05-01 14:57:12 -0700

    Add CORS.

--- app/Global.java
+++ app/Global.java
@@ -1,10 +1,37 @@
-import java.io.IOException;
-
-import play.*;
 import models.PlayConfig;
 import models.Util;
+import play.Application;
+import play.GlobalSettings;
+import play.libs.F.Promise;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import java.io.IOException;
 
 public class Global extends GlobalSettings {
+
+	// http://stackoverflow.com/questions/25152277/play-framework-2-3-cors-headers/26604110#26604110
+	// For CORS
+	private class ActionWrapper extends Action.Simple {
+		public ActionWrapper(Action<?> action) {
+			this.delegate = action;
+		}
+
+		@Override
+		public Promise<Result> call(Http.Context ctx) throws java.lang.Throwable {
+			Promise<Result> result = this.delegate.call(ctx);
+			Http.Response response = ctx.response();
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			return result;
+		}
+	}
+
+	@Override
+	public Action<?> onRequest(Http.Request request, java.lang.reflect.Method actionMethod) {
+		return new ActionWrapper(super.onRequest(request, actionMethod));
+	}
+
 	@Override
 	public void onStart(Application app) {
 		try {
